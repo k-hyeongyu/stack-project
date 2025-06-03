dbfe445cfd844b8722bef4d8f45f7d69db22bc67
k-hyeongyu <kampher@naver.com>
2025-06-04 00:50:52 +0900

    projectBulk250604

--- src/bulkUp/BulkUp.js
+++ src/bulkUp/BulkUp.js
@@ -6,7 +6,10 @@ function BulkUp() { // 컴포넌트 이름은 대문자로 시작해야 합니�
         <div className="container">
             <header className="header">
                 <div className="logo-section">
-                    <div className="logo-box">로고</div>
+                    <div className="logo">
+                        <img src="../BalancEat_logo.png" alt="로고 이미지" />
+                    </div>
+
                     <h1>벌크업</h1>
                 </div>
                 <div className="user-info">
@@ -22,15 +25,15 @@ function BulkUp() { // 컴포넌트 이름은 대문자로 시작해야 합니�
 
                 </aside>
                 <section className="content-grid">
-                    <div className="grid-item content-header">콘텐츠</div>
-                    <div className="grid-item content-description">
-                        벌크업 컨텐츠는 식단과 운동이<br/> {/* JSX에서는 <br> 대신 <br/> 사용 */}
-                        2 : 8의 비율로 된 컨텐츠가 생성
-                    </div>
-                    {/* 나머지 빈 박스들 */}
-                    {[...Array(7)].map((_, index) => ( // 7개의 빈 박스 동적 생성
-                        <div key={index} className="grid-item empty-box"></div>
-                    ))}
+                    <div className="grid-item"></div>
+                    <div className="grid-item"></div>
+                    <div className="grid-item"></div>
+                    <div className="grid-item"></div>
+                    <div className="grid-item"></div>
+                    <div className="grid-item"></div>
+                    <div className="grid-item"></div>
+                    <div className="grid-item"></div>
+                    <div className="grid-item"></div>
                 </section>
             </main>
         </div>
