1c2e5c24e75b043a80099b39808151a7232a6268
dlgkdhq <dlgkdms920@naver.com>
2025-06-04 00:13:11 +0900

    아이디, 비밀번호 찾기 페이지 완성

--- src/login/FindId.js
+++ src/login/FindId.js
@@ -1,9 +1,63 @@
+import { useState } from 'react';
 import './Login.css';
+import { useNavigate } from 'react-router';
 
 function FindId() {
+
+    let [name, setName] = useState('')
+    let [phNumber, setPhNumber] = useState('')
+    let [foundId, setFoundId] = useState('')
+
+    let navigate = useNavigate();
+
     return (
         <div>
-            <p>아이디 찾기</p>
+            <div className='login-container'>
+                <h1 className='oleo-script-regular'>BalancEat</h1>
+                <p className='find-font'> - 아이디 찾기 - </p>
+
+                <form className='find-box' onSubmit={(event) => {
+                    event.preventDefault();
+
+                    if (name.trim() === '') {
+                        alert('이름을 입력해주세요.')
+                        return;
+                    }
+
+                    if (phNumber.trim() === '') {
+                        alert('전화번호를 입력해주세요.')
+                        return;
+                    }
+
+                    if (name === '김스택' && phNumber === '01012345678') {
+                        setFoundId('stack123')
+                    } else {
+                        alert('가입정보가 없습니다.')
+                        setFoundId('')
+                        setName('')
+                        setPhNumber('')
+                    }
+
+                }}>
+                    <input type='text' value={name} className='find-textbox' placeholder='이름'
+                        onChange={(event) => {
+                            setName(event.target.value)
+                        }}></input>
+                    <input type='text' value={phNumber} className='find-textbox' placeholder='전화번호 ( - 없이 숫자만 입력)'
+                        onChange={(event) => {
+                            setPhNumber(event.target.value)
+                        }}></input>
+                    <div className='find-seperator-line'></div>
+                    <button type='submit' className='find-button'>아이디 찾기</button>
+                </form>
+            </div>
+            {
+                foundId && (
+                    <div className='foundId-text'>
+                        <p>{name}님의 아이디는 <strong>{foundId}</strong>입니다.</p>
+                    </div>
+                )
+            }
         </div>
     )
 }
--- src/login/FindPw.js
+++ src/login/FindPw.js
@@ -1,9 +1,63 @@
+import { useState } from 'react';
 import './Login.css';
+import { useNavigate } from 'react-router';
 
 function FindPw() {
+
+    let [name, setName] = useState('')
+    let [id, setId] = useState('')
+    let [foundPw, setFoundPw] = useState('')
+
+    let navigate = useNavigate();
+    
     return (
         <div>
-            <p>비밀번호 찾기</p>
+            <div className='login-container'>
+                <h1 className='oleo-script-regular'>BalancEat</h1>
+                <p className='find-font'> - 비밀번호 찾기 - </p>
+
+                <form className='find-box' onSubmit={(event) => {
+                    event.preventDefault();
+
+                    if (name.trim() === '') {
+                        alert('이름을 입력해주세요.')
+                        return;
+                    }
+
+                    if (id.trim() === '') {
+                        alert('아이디를 입력해주세요.')
+                        return;
+                    }
+
+                    if (name === '김스택' && id === 'stack123') {
+                        setFoundPw('stack1234')
+                    } else {
+                        alert('가입정보가 없습니다.')
+                        setFoundPw('')
+                        setName('')
+                        setId('')
+                    }
+
+                }}>
+                    <input type='text' value={name} className='find-textbox' placeholder='이름'
+                        onChange={(event) => {
+                            setName(event.target.value)
+                        }}></input>
+                    <input type='text' value={id} className='find-textbox' placeholder='아이디'
+                        onChange={(event) => {
+                            setId(event.target.value)
+                        }}></input>
+                    <div className='find-seperator-line'></div>
+                    <button type='submit' className='find-button'>비밀번호 찾기</button>
+                </form>
+            </div>
+            {
+                foundPw && (
+                    <div className='foundId-text'>
+                        <p>{name}님의 비밀번호는 <strong>{foundPw}</strong>입니다.</p>
+                    </div>
+                )
+            }
         </div>
     )
 }
