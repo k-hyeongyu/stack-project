7f0439239ea1a344d02503e40aef503f928de24c
dlgkdhq <dlgkdms920@naver.com>
2025-06-13 09:10:47 +0900

    로그인 페이지 Footer 추가

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,6 +1,8 @@
 import './Login.css';
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
+import Footer from '../health-pages/Footer';
+import '../health-pages/Footer.css';
 
 function Login() {
 
@@ -10,49 +12,52 @@ function Login() {
     let navigate = useNavigate();
 
     return (
-        <div className='login-page-container page-font'>
-            <div className='login-container'>
-                <h1 className='oleo-script-regular login-cursor' onClick={() => {
-                    navigate('/main')
-                }}>BalancEat</h1>
-                <br></br>
-                <form className='login-box' onSubmit={(event) => {
-                    event.preventDefault();
+        <>
+            <div className='login-page-container page-font'>
+                <div className='login-container'>
+                    <h1 className='oleo-script-regular login-cursor' onClick={() => {
+                        navigate('/main')
+                    }}>BalancEat</h1>
+                    <br></br>
+                    <form className='login-box' onSubmit={(event) => {
+                        event.preventDefault();
 
-                    if (inputId === 'stack123' && inputPw === 'stack1234') {
-                        navigate('/mypage/dashboard');
-                        setInputId('')
-                        setInputPw('')
-                    } else {
-                        alert('아이디 또는 비밀번호가 올바르지 않습니다.')
-                        setInputId('')
-                        setInputPw('')
-                    }
+                        if (inputId === 'stack123' && inputPw === 'stack1234') {
+                            navigate('/mypage/dashboard');
+                            setInputId('')
+                            setInputPw('')
+                        } else {
+                            alert('아이디 또는 비밀번호가 올바르지 않습니다.')
+                            setInputId('')
+                            setInputPw('')
+                        }
 
-                }}>
-                    <input type='text' value={inputId} className='login-textbox' placeholder='아이디'
-                        onChange={(event) => {
-                            setInputId(event.target.value)
-                            console.log(event.target.value)
-                        }}></input>
-                    <input type='password' value={inputPw} className='login-textbox' placeholder='비밀번호'
-                        onChange={(event) => {
-                            setInputPw(event.target.value)
-                        }}></input>
+                    }}>
+                        <input type='text' value={inputId} className='login-textbox' placeholder='아이디'
+                            onChange={(event) => {
+                                setInputId(event.target.value)
+                                console.log(event.target.value)
+                            }}></input>
+                        <input type='password' value={inputPw} className='login-textbox' placeholder='비밀번호'
+                            onChange={(event) => {
+                                setInputPw(event.target.value)
+                            }}></input>
 
-                    <button type='submit' className='login-button'>로그인</button>
-                    <div className='seperator-line'></div>
+                        <button type='submit' className='login-button'>로그인</button>
+                        <div className='seperator-line'></div>
 
-                    <div className='login-find'>
-                        <span className='login-cursor' onClick={() => { navigate('/SignUp') }}>회원가입</span>
-                        <span> | </span>
-                        <span className='login-cursor' onClick={() => { navigate('/FindId') }}>아이디 찾기</span>
-                        <span> | </span>
-                        <span className='login-cursor' onClick={() => { navigate('/FindPw') }}>비밀번호 찾기</span>
-                    </div>
-                </form>
+                        <div className='login-find'>
+                            <span className='login-cursor' onClick={() => { navigate('/SignUp') }}>회원가입</span>
+                            <span> | </span>
+                            <span className='login-cursor' onClick={() => { navigate('/FindId') }}>아이디 찾기</span>
+                            <span> | </span>
+                            <span className='login-cursor' onClick={() => { navigate('/FindPw') }}>비밀번호 찾기</span>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
+            <Footer />
+        </>
     )
 }
 
--- src/login/SignUp.js
+++ src/login/SignUp.js
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router';
 import './Login.css';
 import { useState } from 'react';
+import Footer from '../health-pages/Footer';
 
 function SignUp() {
 
@@ -14,65 +15,68 @@ function SignUp() {
     let navigate = useNavigate();
 
     return (
-        <div className='login-page-container'>
-            <div className='signUp-container'>
-                <h1 className='oleo-script-regular login-cursor' onClick={() => {
-                    navigate('/main')
-                }}>BalancEat</h1>
-                <form className='signUp-box' onSubmit={(event) => {
-                    event.preventDefault();
+        <>
+            <div className='login-page-container'>
+                <div className='signUp-container'>
+                    <h1 className='oleo-script-regular login-cursor' onClick={() => {
+                        navigate('/main')
+                    }}>BalancEat</h1>
+                    <form className='signUp-box' onSubmit={(event) => {
+                        event.preventDefault();
 
-                    if (values.name.trim() === '') {
-                        alert('이름을 입력해주세요.');
-                        return;
-                    }
-                    if (values.id.trim() === '') {
-                        alert('아이디를 입력해주세요.');
-                        return;
-                    }
-                    if (values.pw.trim() === '') {
-                        alert('비밀번호를 입력해주세요.');
-                        return;
-                    }
-                    if (values.confirmPw.trim() === '') {
-                        alert('비밀번호를 확인해주세요.');
-                        return;
-                    }
-                    if (values.pw !== values.confirmPw) {
-                        alert('비밀번호가 일치하지 않습니다.');
-                        return;
-                    }
-                    if (values.phoneNumber.trim() === '') {
-                        alert('전화번호를 입력해주세요.');
-                        return;
-                    }
+                        if (values.name.trim() === '') {
+                            alert('이름을 입력해주세요.');
+                            return;
+                        }
+                        if (values.id.trim() === '') {
+                            alert('아이디를 입력해주세요.');
+                            return;
+                        }
+                        if (values.pw.trim() === '') {
+                            alert('비밀번호를 입력해주세요.');
+                            return;
+                        }
+                        if (values.confirmPw.trim() === '') {
+                            alert('비밀번호를 확인해주세요.');
+                            return;
+                        }
+                        if (values.pw !== values.confirmPw) {
+                            alert('비밀번호가 일치하지 않습니다.');
+                            return;
+                        }
+                        if (values.phoneNumber.trim() === '') {
+                            alert('전화번호를 입력해주세요.');
+                            return;
+                        }
 
-                    alert('가입이 완료되었습니다!')
-                    navigate('/login')
-                }}>
-                    <input type='text' name='name' value={values.name} onChange={handleChange} placeholder='이름' className='login-textbox'></input>
-                    <div className='signUp-id'>
-                        <input type='text' name='id' value={values.id} onChange={handleChange} placeholder='아이디' className='login-textbox'></input>
-                        <button type='button' className='confirm-button' onClick={() => {
+                        alert('가입이 완료되었습니다!')
+                        navigate('/login')
+                    }}>
+                        <input type='text' name='name' value={values.name} onChange={handleChange} placeholder='이름' className='login-textbox'></input>
+                        <div className='signUp-id'>
+                            <input type='text' name='id' value={values.id} onChange={handleChange} placeholder='아이디' className='login-textbox'></input>
+                            <button type='button' className='confirm-button' onClick={() => {
 
-                            if (values.id.trim() === '') {
-                                alert('아이디를 입력해주세요.');
-                                return;
-                            }
-                            alert('확인되었습니다.')
+                                if (values.id.trim() === '') {
+                                    alert('아이디를 입력해주세요.');
+                                    return;
+                                }
+                                alert('확인되었습니다.')
 
-                        }}>확인</button>
-                    </div>
+                            }}>확인</button>
+                        </div>
 
-                    <input type='password' name='pw' value={values.pw} onChange={handleChange} placeholder='비밀번호' className='login-textbox'></input>
-                    <input type='password' name='confirmPw' value={values.confirmPw} onChange={handleChange} placeholder='비밀번호 확인' className='login-textbox'></input>
-                    <input type='text' name='email' value={values.email} onChange={handleChange} placeholder='이메일 (선택)' className='login-textbox'></input>
-                    <input type='text' name='phoneNumber' value={values.phoneNumber} onChange={handleChange} placeholder='전화번호 ( - 없이 숫자만 입력)' className='login-textbox'></input>
+                        <input type='password' name='pw' value={values.pw} onChange={handleChange} placeholder='비밀번호' className='login-textbox'></input>
+                        <input type='password' name='confirmPw' value={values.confirmPw} onChange={handleChange} placeholder='비밀번호 확인' className='login-textbox'></input>
+                        <input type='text' name='email' value={values.email} onChange={handleChange} placeholder='이메일 (선택)' className='login-textbox'></input>
+                        <input type='text' name='phoneNumber' value={values.phoneNumber} onChange={handleChange} placeholder='전화번호 ( - 없이 숫자만 입력)' className='login-textbox'></input>
 
-                    <button type='submit' className='signUp-button'>가입 완료</button>
-                </form>
+                        <button type='submit' className='signUp-button'>가입 완료</button>
+                    </form>
+                </div>
             </div>
-        </div>
+            <Footer />
+        </>
     )
 }
 
